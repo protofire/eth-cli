17bf183331cf645da8fac7c3787201625bbd4748
Mariano A <mariano_aguero@hotmail.com>
2018-06-14 09:33:00 -0300

    Prettier applied to the code

--- src/loadContract.js
+++ src/loadContract.js
@@ -18,21 +18,18 @@ module.exports = function(abiPath, address, url) {
   try {
     abi = JSON.parse(abiStr)
 
-    // Allow using truffle artifacts files too. 
+    // Allow using truffle artifacts files too.
     // If abi variable it's an object and it has an abi property, interpret it as a truffle artifact
-    if(abi !== null && typeof abi === 'object' && abi.abi) {
-      abi = abi.abi;
+    if (abi !== null && typeof abi === 'object' && abi.abi) {
+      abi = abi.abi
     }
-
   } catch (e) {
     console.log('Error parsing abi', e)
     process.exit(1)
   }
 
   // Connect web3
-  const web3 = new Web3(
-    new Web3.providers.HttpProvider(url)
-  )
+  const web3 = new Web3(new Web3.providers.HttpProvider(url))
 
   // Get contract
   const Contract = new web3.eth.Contract(abi, address)
