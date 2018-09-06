01515711ff0eb3092dcd7872c0ae764f77c82738
Mariano Aguero <mariano.aguero@altoros.com>
2018-09-06 12:28:10 -0300

    Convert function to constant, add network urls constant

--- src/networks.js
+++ src/networks.js
@@ -1,10 +1,18 @@
-module.exports.getIds = () => {
-  return {
-    mainnet: 1,
-    ropsten: 3,
-    rinkeby: 4,
-    kovan: 42,
-    sokol: 77,
-    poa: 99
-  }
+module.exports.networksIds = {
+  mainnet: 1,
+  ropsten: 3,
+  rinkeby: 4,
+  kovan: 42,
+  sokol: 77,
+  poa: 99
+}
+
+module.exports.networksUrls = {
+  mainnet: 'https://mainnet.infura.io',
+  ropsten: 'https://ropsten.infura.io',
+  rinkeby: 'https://rinkeby.infura.io',
+  kovan: 'https://kovan.infura.io',
+  sokol: 'https://sokol.poa.network',
+  poa: 'https://core.poa.network',
+  local: 'http://localhost:8545'
 }
