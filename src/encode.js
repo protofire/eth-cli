87dbc5059682c916ee4c7fdbd2a1e79980fa1ef6
fernandomg <fernando.greco@gmail.com>
2018-10-29 15:59:53 -0300

    Add `encode` method

---
+++ src/encode.js
@@ -0,0 +1,33 @@
+const { extractMethodObjectsFromABI, loadABI } = require('./utils')
+const Web3 = require('web3')
+
+module.exports = function(abiPath, methodCall, url) {
+  if (!methodCall) {
+    throw new Error('[encode] methodCall required')
+  }
+
+  const isValidMethod = /^(\w+)\((.*)\)$/
+  const method = isValidMethod.exec(methodCall)
+
+  if (!method) {
+    throw new Error('[encode] methodCall invalid structure')
+  }
+
+  const methodName = method[1]
+  const abi = loadABI(abiPath)
+  const matchingMethods = extractMethodObjectsFromABI(abi).filter(x => x.name === methodName)
+
+  if (matchingMethods.length > 1) {
+    throw new Error('[encode] function overloading is not supported in the current version')
+  }
+
+  if (!matchingMethods.length) {
+    throw new Error('[encode] method specified does not exist in the ABI file provided')
+  }
+
+  const web3 = new Web3(new Web3.providers.HttpProvider(url))
+  // `contract` is being used as part of the eval call
+  // eslint-disable-next-line no-unused-vars
+  const contract = new web3.eth.Contract(abi)
+  return eval(`contract.methods.${methodCall}.encodeABI()`)
+}
