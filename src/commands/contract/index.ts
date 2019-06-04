160db56c5d7cd2b731783cf694023ca559a78b2b
fvictorio <victorio.franco@gmail.com>
2019-06-04 15:09:36 -0300

    Move contract:load functionality to the repl command

--- src/commands/contract/index.ts
+++ src/commands/contract/index.ts
@@ -7,7 +7,6 @@ export default class ContractCommand extends Command {
 
   static examples = [
     'eth contract:deploy --ropsten 3daa79a26454a5528a3523f9e6345efdbd636e63f8c24a835204e6ccb5c88f9e ./contracts/proxy.bin',
-    'eth contract:load ./contracts/proxy.abi 0x601fd71f284B1933420A5DB0C43B10efC429A008',
     'eth contract:address 0x92970dbD5C0Ee6b439422bFd7cD71e1DDA921A03',
   ]
 
