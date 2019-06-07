8d00c5132e4296b2bb4eb1453711de06c6e0aedd
fvictorio <victorio.franco@gmail.com>
2019-06-07 15:14:49 -0300

    Add pk flag to method:send-transaction

--- src/commands/method/send-transaction.ts
+++ src/commands/method/send-transaction.ts
@@ -1,12 +1,14 @@
 import { cli } from 'cli-ux'
 
 import { BaseCommand } from '../../base'
+import { privateKeyFlag } from '../../flags'
 
 export default class SendTransactionCommand extends BaseCommand {
-  static description = `Sends the transaction for the contract in <address> with <encodedABI> using private key <pk>.`
+  static description = `Sends the transaction for the contract in <address> with <encodedABI> using the given private key.`
 
   static flags = {
     ...BaseCommand.flags,
+    pk: { ...privateKeyFlag, required: true },
   }
 
   static args = [
@@ -20,14 +22,9 @@ export default class SendTransactionCommand extends BaseCommand {
       required: true,
       description: `The contract's address.`,
     },
-    {
-      name: 'pk',
-      required: true,
-      description: 'The private key.',
-    },
   ]
 
-  static examples = ['eth method:send-transaction']
+  static examples = ['eth method:send-transaction --pk 0x6db0bdfc7800dcf87b5a88b3363997360395d36ef51db10c3458d51d8aefd37e 0xa9059cbb000000000000000000000000b2e4a264a982039f8e503ea3c83af5537f583069000000000000000000000000da480b4852ca3ade4acf3eeca6901952edbae912 0x15503FBAb2fa57535092ab9c24740142Ab6cabd3']
 
   static aliases = ['m:st']
 
@@ -39,9 +36,10 @@ export default class SendTransactionCommand extends BaseCommand {
     try {
       networkUrl = this.getNetworkUrl(flags)
 
-      const { encodedABI, address, pk } = args
+      const { encodedABI, address } = args
+      const { pk } = flags
       const { sendTransaction } = await import('../../helpers/sendTransaction')
-      const result = await sendTransaction(encodedABI, address, pk, networkUrl)
+      const result = await sendTransaction(encodedABI, address, pk!, networkUrl)
 
       cli.styledJSON(result)
     } catch (e) {
