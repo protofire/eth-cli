fe7bad5e5d7b8a2034a3581af9b5f090930f2e21
Franco Victorio <victorio.franco@gmail.com>
2019-08-28 11:57:55 -0300

    feat: add --confirmation-blocks flag to contract:deploy

--- src/helpers/deploy.ts
+++ src/helpers/deploy.ts
@@ -1,27 +1,14 @@
 import * as fs from 'fs'
 import Web3 from 'web3'
-import Contract from 'web3/eth/contract'
-import { TransactionReceipt } from 'web3/types'
-import { Unit } from 'web3/utils'
 
 import { getPrivateKey } from './config'
 import { add0x } from './utils'
 
-interface DeployResult {
-  receipt: TransactionReceipt
-  address: string
-}
-
-export function deploy(
+export async function deploy(
   url: string,
   privateKeyOrKnownAddress: string,
   binPath: string,
-): Promise<DeployResult> {
-  const transactionConfirmationBlocks = 3
-  const options = {
-    transactionConfirmationBlocks,
-  }
-
+): Promise<string> {
   const web3 = new Web3(new Web3.providers.HttpProvider(url))
   const privateKey = getPrivateKey(privateKeyOrKnownAddress)
 
@@ -30,35 +17,26 @@ export function deploy(
   const data = add0x(fs.readFileSync(binPath).toString())
 
   const Contract: any = web3.eth.Contract // ts hack: transactionConfirmationBlocks is not a valid option
-  const contract = new Contract([], undefined, options)
+  const contract = new Contract([])
 
   const deploy = contract.deploy({ data })
-  let receipt: TransactionReceipt
 
-  return deploy
-    .estimateGas({
-      from: address,
-    })
-    .then((gas: Unit) => {
-      process.stderr.write(`Estimated gas: ${gas}\n`)
+  const gas = await deploy.estimateGas({
+    from: address,
+  })
 
-      return deploy
-        .send({
-          from: address,
-          gas,
-        })
-        .on('transactionHash', (tx: string) => {
-          process.stderr.write(`TX: ${tx}\n`)
-        })
-        .on(
-          'confirmation',
-          (confirmationNumber: number, transactionReceipt: TransactionReceipt) => {
-            receipt = transactionReceipt
-            process.stderr.write(
-              `Confirmation ${confirmationNumber} of ${transactionConfirmationBlocks}\n`,
-            )
-          },
-        )
-        .then((contract: Contract) => ({ address: contract.options.address, receipt }))
-    })
+  return new Promise((resolve, reject) => {
+    deploy.send(
+      {
+        from: address,
+        gas,
+      },
+      (err: Error, txHash: string) => {
+        if (err) {
+          return reject(err)
+        }
+        return resolve(txHash)
+      },
+    )
+  })
 }
