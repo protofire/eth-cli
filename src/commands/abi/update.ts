ca028a9d8fae58007ca57cb8841b91402cc32b75
Franco Victorio <victorio.franco@gmail.com>
2019-10-07 15:44:33 -0300

    Add abi:update command

--- src/commands/abi/update.ts
+++ src/commands/abi/update.ts
@@ -4,13 +4,13 @@ import { isEmptyCommand } from '../../helpers/checkCommandInputs'
 import { config } from '../../helpers/config'
 import { loadABI } from '../../helpers/utils'
 
-export class AddCommand extends Command {
-  static description = 'Add a known ABI'
+export class UpdateCommand extends Command {
+  static description = 'Update a known ABI'
 
   static args = [
     {
       name: 'name',
-      required: false,
+      required: true,
       description: 'Name of the ABI to add',
     },
     {
@@ -20,10 +20,10 @@ export class AddCommand extends Command {
     },
   ]
 
-  static examples = ['eth abi:add erc777 ./path/to/erc777.json']
+  static examples = ['eth abi:update erc777 ./path/to/erc777.json']
 
   async run() {
-    const { args, flags } = this.parse(AddCommand)
+    const { args, flags } = this.parse(UpdateCommand)
 
     if (isEmptyCommand(flags, args)) {
       this._help()
@@ -36,11 +36,11 @@ export class AddCommand extends Command {
       const abis = config.get('abis', [])
       const abi = loadABI(abiPath)
       const index = abis.findIndex((item: any) => item.name.toLowerCase() === name.toLowerCase())
-      if (index === -1) {
-        abis.push({ name, abi })
+      if (index !== -1) {
+        abis[index] = { name, abi }
         config.set('abis', abis)
       } else {
-        this.warn(`ABI '${name}' already exists. Use abi:update if you want to modify it.`)
+        this.warn(`No ABI found for '${name}'`)
       }
     } catch (e) {
       this.error(e.message, { exit: 1 })
