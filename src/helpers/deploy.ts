79ff6d0b1e789d48e15cd3c7a3d8e2eff3f3097c
Franco Victorio <victorio.franco@gmail.com>
2019-10-17 09:19:14 -0300

    Allow specifying constructor arguments in contract:deploy

--- src/helpers/deploy.ts
+++ src/helpers/deploy.ts
@@ -7,7 +7,9 @@ import { add0x } from './utils'
 export async function deploy(
   url: string,
   privateKeyOrKnownAddress: string,
+  abi: any[],
   binPath: string,
+  constructorArguments: any[],
 ): Promise<string> {
   const web3 = new Web3(new Web3.providers.HttpProvider(url))
   const networkId = await web3.eth.net.getId()
@@ -18,9 +20,9 @@ export async function deploy(
   const data = add0x(fs.readFileSync(binPath).toString())
 
   const Contract: any = web3.eth.Contract // ts hack: transactionConfirmationBlocks is not a valid option
-  const contract = new Contract([])
+  const contract = new Contract(abi)
 
-  const deploy = contract.deploy({ data })
+  const deploy = contract.deploy({ data, arguments: constructorArguments })
 
   const gas = await deploy.estimateGas({
     from: address,
